60b8ca02bac83a02903e59211b74d24f052cda12
Tobias Schmid <tobias.schmid@fau.de>
2022-11-06 21:37:27 +0100

    Implemented and tested issue #18
    Signed-off-by: Tobias Schmid <tobias.schmid@fau.de>

---
+++ App/automotive/src/main/java/com/amos/infotaimos/LandingPage.kt
@@ -0,0 +1,37 @@
+package com.amos.infotaimos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.amos.infotaimos.databinding.FragmentLandingPageBinding
+
+
+class LandingPage : Fragment() {
+
+    private var binding: FragmentLandingPageBinding? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val bindingObject = FragmentLandingPageBinding.inflate(inflater, container, false)
+        binding = bindingObject
+        return bindingObject.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+           landingPage = this@LandingPage
+        }
+    }
+    fun goToNavigationScreen() {
+        findNavController().navigate(R.id.action_landingPage_to_navigationPage)
+    }
+
+}
