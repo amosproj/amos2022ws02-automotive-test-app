15a9535bf1e5408e9e0968cd4f125d76a3feb3f8
Daniel Lang <dlang8428@gmail.com>
2022-11-07 22:40:25 +0100

    Added CarAppFpcusManager.OnAppFocusOwnershipCallback Co-authored-by: Emre Güder <emrer17m@gmail.com>
    Signed-off-by: Daniel Lang <dlang8428@gmail.com>

--- App/automotive/src/main/java/com/amos/infotaimos/Model.kt
+++ App/automotive/src/main/java/com/amos/infotaimos/Model.kt
@@ -9,6 +9,22 @@ class Model(context: Context) {
     private var car = Car.createCar(context)
     private var carAppFocusManager = car.getCarManager(Car.APP_FOCUS_SERVICE) as CarAppFocusManager
     fun startNavigation(delay: Int) {
-        println(carAppFocusManager.requestAppFocus(CarAppFocusManager.APP_FOCUS_TYPE_NAVIGATION, null))
+        val navCallback = NavCallback()
+        println(carAppFocusManager.requestAppFocus(CarAppFocusManager.APP_FOCUS_TYPE_NAVIGATION, navCallback))
+    }
+
+    class NavCallback : CarAppFocusManager.OnAppFocusOwnershipCallback{
+        override fun onAppFocusOwnershipLost(appType: Int) {
+            if(appType == CarAppFocusManager.APP_FOCUS_TYPE_NAVIGATION)
+                return
+            TODO("Not yet implemented")
+        }
+
+        override fun onAppFocusOwnershipGranted(appType: Int) {
+            if(appType == CarAppFocusManager.APP_FOCUS_TYPE_NAVIGATION)
+                return
+            TODO("Not yet implemented")
+        }
+
     }
 }
