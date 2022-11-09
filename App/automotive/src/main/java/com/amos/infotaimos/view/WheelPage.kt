a681a406f56ff17976b1dbdbb54dd4c2f9e99cc3
Anders Hausding <ahausding@sensorstim.com>
2022-11-09 15:00:26 +0100

    implement click dummy view

---
+++ App/automotive/src/main/java/com/amos/infotaimos/view/WheelPage.kt
@@ -0,0 +1,17 @@
+package com.amos.infotaimos.view
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.amos.infotaimos.ViewBindingFragment
+import com.amos.infotaimos.databinding.FragmentWheelPageBinding
+import com.amos.infotaimos.viewmodel.WheelPageViewModel
+
+class WheelPage : ViewBindingFragment<FragmentWheelPageBinding>() {
+    private val viewModel: WheelPageViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //TODO button actions
+    }
+}
