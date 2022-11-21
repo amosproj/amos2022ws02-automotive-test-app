dac4ad8a787615fa4f17d37ffa0e02985cb3bc20
hy37dywe <berkan.tuncay@fau.de>
2022-11-21 20:33:18 +0100

    Added navigation unit tests to landing page

--- App/automotive/src/androidTest/java/com/amos/infotaimos/LandingPageTest.kt
+++ App/automotive/src/androidTest/java/com/amos/infotaimos/LandingPageTest.kt
@@ -15,21 +15,112 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class LandingPageTest {
 
+    // https://developer.android.com/guide/navigation/navigation-testing
     @Test
-    fun testNavigationToNavigationScreen() {
+    fun testNavigationToNavigationPage() {
+        // Create a TestNavHostController
         val navController = TestNavHostController(
-            ApplicationProvider.getApplicationContext()
-        )
+            ApplicationProvider.getApplicationContext())
 
-        val landingScenario = launchFragmentInContainer<LandingPage>()
+        // Create a graphical FragmentScenario for the TitleScreen
+        val landingPageScenario = launchFragmentInContainer<LandingPage>()
 
-        landingScenario.onFragment { fragment ->
+        landingPageScenario.onFragment { fragment ->
+            // Set the graph on the TestNavHostController
             navController.setGraph(R.navigation.nav_graph)
 
+            // Make the NavController available via the findNavController() APIs
             Navigation.setViewNavController(fragment.requireView(), navController)
         }
 
-        onView(ViewMatchers.withId(R.id.Navigation)).perform(ViewActions.click())
+        // Verify that performing a click changes the NavController’s state
+        onView(ViewMatchers.withId(R.id.cardViewNavigation)).perform(ViewActions.click())
         assertEquals(navController.currentDestination?.id, R.id.navigationPage)
+    }
+
+    @Test
+    fun testNavigationToNavigationPageIcon() {
+        // Create a TestNavHostController
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext())
+
+        // Create a graphical FragmentScenario for the TitleScreen
+        val landingPageScenario = launchFragmentInContainer<LandingPage>()
+
+        landingPageScenario.onFragment { fragment ->
+            // Set the graph on the TestNavHostController
+            navController.setGraph(R.navigation.nav_graph)
+
+            // Make the NavController available via the findNavController() APIs
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        // Verify that performing a click changes the NavController’s state
+        onView(ViewMatchers.withId(R.id.)).perform(ViewActions.click())
+        assertEquals(navController.currentDestination?.id, R.id.navigationPage)
+    }
+
+    @Test
+    fun testNavigationToWheelPage() {
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext())
+
+        val landingPageScenario = launchFragmentInContainer<LandingPage>()
+
+        landingPageScenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        onView(ViewMatchers.withId(R.id.cardViewWheel)).perform(ViewActions.click())
+        assertEquals(navController.currentDestination?.id, R.id.wheelPage)
+    }
+
+    @Test
+    fun testNavigationToMediaPage() {
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext())
+
+        val landingPageScenario = launchFragmentInContainer<LandingPage>()
+
+        landingPageScenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        onView(ViewMatchers.withId(R.id.cardViewMedia)).perform(ViewActions.click())
+        assertEquals(navController.currentDestination?.id, R.id.mediaPage)
+    }
+
+    @Test
+    fun testNavigationToVehiclePropertiesPage() {
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext())
+
+        val landingPageScenario = launchFragmentInContainer<LandingPage>()
+
+        landingPageScenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        onView(ViewMatchers.withId(R.id.cardViewVehicleProperties)).perform(ViewActions.click())
+        assertEquals(navController.currentDestination?.id, R.id.vehicleProperties)
+    }
+
+    @Test
+    fun testNavigationToPowerManagement() {
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext())
+
+        val landingPageScenario = launchFragmentInContainer<LandingPage>()
+
+        landingPageScenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        onView(ViewMatchers.withId(R.id.cardViewPowerManagement)).perform(ViewActions.click())
+        assertEquals(navController.currentDestination?.id, R.id.powerManagement)
     }
 }
