f7b0ada5bb72320573ac202fea1e75857923de43
hanna-212 <mueller.hanna96@gmail.com>
2022-11-28 23:33:16 +0100

    changed design, added test
    Signed-off-by: hanna-212 <mueller.hanna96@gmail.com>

--- App/automotive/src/androidTest/java/com/amos/infotaimos/PowerManagementPageTest.kt
+++ App/automotive/src/androidTest/java/com/amos/infotaimos/PowerManagementPageTest.kt
@@ -3,8 +3,12 @@ package com.amos.infotaimos
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import com.amos.infotaimos.view.PowerManagementPage
+import com.amos.infotaimos.view.WheelPage
 
 import org.junit.Test
 
@@ -15,5 +19,7 @@ class PowerManagementPageTest {
         launchFragmentInContainer<PowerManagementPage>()
         onView(ViewMatchers.withId(R.id.fragment_power_button_display_off)).perform(click())
         onView(ViewMatchers.withId(R.id.fragment_power_button_stand_by)).perform(click())
+        onView(ViewMatchers.withId(R.id.fragment_power_button_delay)).check(matches(isDisplayed()))
+        onView(ViewMatchers.withId(R.id.delay_spinner_power_management_page)).check(matches(isDisplayed())).perform(click())
     }
 }
