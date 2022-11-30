8a218e8e6bcdb6db94b42f0e0da10e551c1b6ba8
Tobias Schmid <tobias.schmid@fau.de>
2022-11-30 14:42:00 +0100

    add empty timer fragment
    Signed-off-by: Tobias Schmid <tobias.schmid@fau.de>

--- App/automotive/src/main/java/com/amos/infotaimos/view/TimerPage.kt
+++ App/automotive/src/main/java/com/amos/infotaimos/view/TimerPage.kt
@@ -3,8 +3,8 @@ package com.amos.infotaimos.view
 import androidx.fragment.app.viewModels
 import com.amos.infotaimos.ViewBindingFragment
 import com.amos.infotaimos.databinding.FragmentMediaPageBinding
-import com.amos.infotaimos.viewmodel.MediaPageViewModel
+import com.amos.infotaimos.viewmodel.TimerPageViewModel
 
-class MediaPage : ViewBindingFragment<FragmentMediaPageBinding>() {
-    private val viewModel: MediaPageViewModel by viewModels()
+class TimerPage : ViewBindingFragment<FragmentMediaPageBinding>() {
+    private val viewModel: TimerPageViewModel by viewModels()
 }
