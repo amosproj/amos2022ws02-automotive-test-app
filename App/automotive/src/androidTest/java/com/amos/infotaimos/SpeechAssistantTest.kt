9f8dd98eb5dcafef7276fea3aa844af1f6619246
hy37dywe <berkan.tuncay@fau.de>
2022-12-06 22:06:56 +0100

    Changed yellow color

--- App/automotive/src/androidTest/java/com/amos/infotaimos/SpeechAssistantTest.kt
+++ App/automotive/src/androidTest/java/com/amos/infotaimos/SpeechAssistantTest.kt
@@ -2,17 +2,22 @@ package com.amos.infotaimos
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import com.amos.infotaimos.view.SpeechAssistantPage
 import org.junit.Test
+
+
 
 class SpeechAssistantTest {
     @Test
     fun testLayoutCompleteness() {
         launchFragmentInContainer<SpeechAssistantPage>()
-        onView(ViewMatchers.withId(R.id.fragment_speech_announcement_button)).perform(ViewActions.click())
-        onView(ViewMatchers.withId(R.id.fragment_ptt_button)).perform(ViewActions.click())
-        onView(ViewMatchers.withId(R.id.fragment_ttt_button)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.fragment_speech_announcement_button)).perform(click())
+        onView(ViewMatchers.withId(R.id.fragment_ptt_button)).perform(click())
+        onView(ViewMatchers.withId(R.id.fragment_ttt_button)).perform(click())
+        onView(ViewMatchers.withId(R.id.delay_spinner_speech_assistant_page)).check(matches(isDisplayed())).perform(click())
     }
 }
