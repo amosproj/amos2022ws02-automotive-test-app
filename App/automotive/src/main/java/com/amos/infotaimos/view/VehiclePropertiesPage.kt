6d36722e74ba149d1b432de03018cd31b44acffe
LaraSlzb <lara.sulzbach@fau.de>
2022-12-11 15:52:54 +0100

    Made vin editable and storable
    Signed-off-by: LaraSlzb <lara.sulzbach@fau.de>

--- App/automotive/src/main/java/com/amos/infotaimos/view/VehiclePropertiesPage.kt
+++ App/automotive/src/main/java/com/amos/infotaimos/view/VehiclePropertiesPage.kt
@@ -9,15 +9,17 @@ import com.amos.infotaimos.viewmodel.VehiclePropertiesPageViewModel
 
 class VehiclePropertiesPage : ViewBindingFragment<FragmentVehiclePropertiesPageBinding>() {
     private val viewModel: VehiclePropertiesPageViewModel by viewModels()
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        context?.let { viewModel.registerVinCallback(it) }
+        viewModel.loadData(requireContext())
         viewModel.vin.observe(viewLifecycleOwner) { vin ->
             binding.vinTile.tileVinText.text = vin
         }
+
+        binding.vinTile.tileVinEditButton.setOnClickListener {
+            viewModel._vin.value = binding.vinTile.tileVinEditableText.text.toString()
+            viewModel.saveData(requireContext(), binding.vinTile.tileVinText.text.toString())
+            binding.vinTile.tileVinEditableText.text.clear()
+        }
     }
-
-
 }
--- App/automotive/src/main/java/com/amos/infotaimos/viewmodel/VehiclePropertiesPageViewModel.kt
+++ App/automotive/src/main/java/com/amos/infotaimos/viewmodel/VehiclePropertiesPageViewModel.kt
@@ -1,45 +1,22 @@
 package com.amos.infotaimos.viewmodel
 
-import android.car.Car
-import android.car.VehiclePropertyIds
-import android.car.hardware.CarPropertyValue
-import android.car.hardware.property.CarPropertyManager
 import android.content.Context
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.amos.infotaimos.model.CarInstanceManager
+import com.amos.infotaimos.model.VinService
 
 class VehiclePropertiesPageViewModel : ViewModel() {
-    private val _vin: MutableLiveData<String> = MutableLiveData("712587-3567883-5398753")
+
+    val _vin: MutableLiveData<String> = MutableLiveData("")
     val vin: LiveData<String>
         get() = _vin
 
-    fun registerVinCallback(context: Context) {
-        Log.d(TAG, "Registering VIN callback")
-        val car = CarInstanceManager.getCarInstance(context)
-        val carPropertyManager = car.getCarManager(Car.PROPERTY_SERVICE) as CarPropertyManager
-        carPropertyManager.registerCallback(
-            carPropertyListener,
-            VehiclePropertyIds.INFO_VIN,
-            CarPropertyManager.SENSOR_RATE_ONCHANGE
-        )
+    fun loadData(context: Context) {
+        _vin.value = VinService.loadData(context)
     }
 
-    private var carPropertyListener = object : CarPropertyManager.CarPropertyEventCallback {
-        override fun onChangeEvent(value: CarPropertyValue<Any>) {
-            Log.d(TAG, "Received on changed car property event")
-            _vin.value = value.value.toString()
-            Log.d(TAG, "VIN: ${_vin.value}")
-        }
-
-        override fun onErrorEvent(propId: Int, zone: Int) {
-            Log.w(TAG, "Received error car property event, propId=$propId")
-        }
-    }
-
-    companion object {
-        const val TAG = "VehiclePropertiesPage"
+    fun saveData(context: Context, s: String) {
+        VinService.saveData(context, s)
     }
 }
