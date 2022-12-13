56c9712cd58efba0671618048944f20cb8dfe629
Emre Güder <emrer17m@gmail.com>
2022-12-13 20:15:44 +0100

    change test type

--- App/automotive/src/androidTest/java/com/amos/infotaimos/VehiclePropertiesPageTest.kt
+++ App/automotive/src/androidTest/java/com/amos/infotaimos/VehiclePropertiesPageTest.kt
@@ -3,6 +3,7 @@ package com.amos.infotaimos
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import com.amos.infotaimos.view.VehiclePropertiesPage
@@ -16,7 +17,7 @@ class VehiclePropertiesPageTest {
         onView(ViewMatchers.withId(R.id.tile_vehicle_theme_status_icon)).perform(click())
         onView(ViewMatchers.withId(R.id.tile_vehicle_range_icon)).perform(click())
         onView(ViewMatchers.withId(R.id.battery_low_message_button)).perform(click())
-        onView(ViewMatchers.withId(R.id.battery_level_edittext)).perform(click())
+        onView(ViewMatchers.withId(R.id.battery_level_edittext)).perform(typeText("Test"))
         onView(ViewMatchers.withId(R.id.delay_spinner_vehicle_properties_page)).check(
             ViewAssertions.matches(
                 ViewMatchers.isDisplayed()
