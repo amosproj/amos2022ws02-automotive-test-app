05fbc8ee03c2bb392074e1b6c9e3c96423ce0cfa
Emre Güder <emrer17m@gmail.com>
2022-12-22 12:26:10 +0100

    implement test drive list

--- App/automotive/src/main/java/com/amos/infotaimos/view/TestDrivePage.kt
+++ App/automotive/src/main/java/com/amos/infotaimos/view/TestDrivePage.kt
@@ -6,10 +6,16 @@ import androidx.fragment.app.viewModels
 import com.amos.infotaimos.R
 import com.amos.infotaimos.ViewBindingFragment
 import com.amos.infotaimos.databinding.FragmentTestDriveBinding
+import com.amos.infotaimos.model.TestDriveItem
+import com.amos.infotaimos.model.TestDriveRecyclerViewAdapter
+import com.amos.infotaimos.model.TimerRecyclerViewAdapter
 import com.amos.infotaimos.viewmodel.TestDrivePageViewModel
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 class TestDrivePage : ViewBindingFragment<FragmentTestDriveBinding>() {
     private val viewModel: TestDrivePageViewModel by viewModels()
+    private lateinit var adapter: TestDriveRecyclerViewAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel._tapText.value = resources.getString(R.string.tap_to_start)
@@ -25,5 +31,7 @@ class TestDrivePage : ViewBindingFragment<FragmentTestDriveBinding>() {
                 viewModel._tapText.value = resources.getString(R.string.tap_to_start)
             }
         }
+        adapter = TestDriveRecyclerViewAdapter()
+        binding.testDriveRecyclerView.adapter = adapter
     }
 }
