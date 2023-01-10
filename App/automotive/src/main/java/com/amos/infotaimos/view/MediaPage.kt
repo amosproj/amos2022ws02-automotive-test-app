e322eb952d24326405c25dbd8c57e050acd96403
hy37dywe <berkan.tuncay@fau.de>
2023-01-10 19:14:37 +0100

    Implemented Media Event List View

--- App/automotive/src/main/java/com/amos/infotaimos/view/MediaPage.kt
+++ App/automotive/src/main/java/com/amos/infotaimos/view/MediaPage.kt
@@ -1,10 +1,20 @@
 package com.amos.infotaimos.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import com.amos.infotaimos.ViewBindingFragment
 import com.amos.infotaimos.databinding.FragmentMediaPageBinding
+import com.amos.infotaimos.model.MediaRecyclerViewAdapter
 import com.amos.infotaimos.viewmodel.MediaPageViewModel
 
 class MediaPage : ViewBindingFragment<FragmentMediaPageBinding>() {
     private val viewModel: MediaPageViewModel by viewModels()
+    private lateinit var adapter: MediaRecyclerViewAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        adapter = MediaRecyclerViewAdapter()
+        binding.mediaEventRecyclerView.adapter = adapter
+    }
 }
